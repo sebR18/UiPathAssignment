42509f310edd23ed96b245ae1e8ee48b42729309
Jhoan Sebastian Ramirez <12sebastian@gmail.com>
2021-10-20 21:20:23 -0700

    initial commit

---
+++ utils.js
@@ -0,0 +1,22 @@
+
+function runAutomation(automationName, arguments) {
+      const robot = UiPathRobot.init(10);
+
+      robot.getProcesses()
+      .then((processes) => {
+        let statusHandler = (status) => {console.log('automation status -' + status)};
+        let automatedProcess = processes.find(p => p.name.includes(automationName));
+
+        automatedProcess.start(arguments).onStatus(statusHandler).then(() => {
+          statusHandler("finished automation");
+        }, err => {
+          console.log(`Could not start automation ${automationName} error: ${err}`);
+        })
+      }, err => {
+        console.log(`Could not find automation ${automationName} error ${err}`);
+      });}
+
+
+      function sendEmail(emailAddress) {
+        runAutomation('Generate.Seahawks.Schedule', {'recipientEmail': emailAddress});
+      }
